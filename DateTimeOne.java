e93fc357e9aef47e0a73122427d6716836abfae0
ItsShuriken <clark.g.smith-1@ou.edu>
2019-10-25 19:21:27 -0500

    Completed few methods fully

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,38 +1,71 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
+	private static int second;
+	private static HashMap<String, String> TIMEZONE_MAP;
+	
+	public DateTimeOne() {
+		LocalDateTime ldt = LocalDateTime.now();
+		second = ldt.getSecond();
+	}
 
-	@Override
+	
+	/*
+	 * Uses LocalDateTime to get current time then uses Date time formatter to 
+	 * format to the desired style.
+	 *
+	 * returns a string.
+	 */
+	public void dateTimeNow() {
+		LocalDateTime ldt = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
+		
+		System.out.println("Current Date/Time: " + ldt.format(formatter));
+	}
+	
 	int getValueOfSecond() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("The value of Second now: " + second);
+		return second;
 	}
 
-	@Override
-	void dateTimeNow() {
-		// TODO Auto-generated method stub
-		
+	void sleepForFiveSec() throws InterruptedException {
+		TimeUnit.SECONDS.sleep(5);
 	}
 
-	@Override
-	void sleepForFiveSec() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
+	
+	/*
+	 * Time on Server: 15:14
+	 * GMT: 20:14
+	 * BST (90E): 2:14
+	 * CST (90W): 15:14
+	 */
 	void dateTimeOfOtherCity() {
-		// TODO Auto-generated method stub
+		ZonedDateTime GMT = ZonedDateTime.now(ZoneId.of("Etc/GMT-0"));
+		ZonedDateTime BST = ZonedDateTime.now(ZoneId.of("Asia/Dhaka"));
+		ZonedDateTime CST = ZonedDateTime.now(ZoneId.of("America/Chicago"));
 		
+		   
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
+		
+		TIMEZONE_MAP = new HashMap<>();		    	
+		// populate Time Zone HashMap
+		TIMEZONE_MAP.put("GMT", "GMT: " + GMT.format(formatter));
+		TIMEZONE_MAP.put("BST", "BST: " + BST.format(formatter));
+		TIMEZONE_MAP.put("CST", "CST: " + CST.format(formatter));
+		
+		for (String key: TIMEZONE_MAP.values())
+			System.out.println(key);
 	}
 
-	@Override
 	void dateTimeDifferentZone() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	void timeZoneHashMap() {
 		// TODO Auto-generated method stub
 		
