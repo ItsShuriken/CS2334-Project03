2569123c2d19530a06f4ec5300fac7ba709202bb
ItsShuriken <clark.g.smith-1@ou.edu>
2019-10-25 19:23:28 -0500

    comments added for alter use

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -2,6 +2,13 @@
 public class MesoAsciiCal extends MesoAsciiAbstract 
 {
 
+	//For MesoAsciiCal, for calAverage, you’re returning the average of two ascii averages: the MesoStation code parameter, and “NRMN”.
+	
+	//I dont think i need this in the end but i do to run the program
+	public MesoAsciiCal(MesoStation mesoStation) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	int calAverage() {
 		// TODO Auto-generated method stub
