b51f53837141fb18390ece6b387c27aaa41835d1
ItsShuriken <clark.g.smith-1@ou.edu>
2019-10-26 19:59:29 -0500

    Completed file

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -2,17 +2,33 @@
 public class MesoAsciiCal extends MesoAsciiAbstract 
 {
 
+	private String stID;
+	private final String fixedSTID = "NRMN";
 	//For MesoAsciiCal, for calAverage, you’re returning the average of two ascii averages: the MesoStation code parameter, and “NRMN”.
 	
 	//I dont think i need this in the end but i do to run the program
 	public MesoAsciiCal(MesoStation mesoStation) {
-		// TODO Auto-generated constructor stub
+		this.stID = mesoStation.getStID();
+	
+	
 	}
 
 	@Override
 	int calAverage() {
-		// TODO Auto-generated method stub
-		return 0;
+		char[] charSTID = stID.toCharArray();
+		char[] charSTIDFixed = fixedSTID.toCharArray();
+		double totalstid = 0;
+		double totalFixed = 0;
+		double finalAvg = 0;
+		
+		for (int i = 0; i < 4; i++) 
+			totalstid += (int)charSTID[i];
+		for (int i = 0; i < 4; i++) 	
+			totalFixed += (int)charSTIDFixed[i];
+		
+		finalAvg = Math.ceil(((totalstid / 4) + (totalFixed / 4)) /2);
+		
+		return (int) finalAvg;
 	}
    
 }
